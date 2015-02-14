ee2773cb6316683303588e52df3b6f87b61202d2
Chris Stokes <stokes84@gmail.com>
2015-02-13 21:40:56 -0800

    Rehaul of installer
    New install system, automated SSL install, removed tips but left comments, cleaned up.

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Clear Screen
+tput clear
+
+# Hide Cursor
+tput civis
+trap tput cnorm EXIT
+
 # Set some styles
-bold=`tput bold`
-alert=`tput setaf 1`
-info=`tput setaf 3`
-normal=`tput sgr0`
+bold=$(tput bold)
+alert=$(tput setaf 1)
+info=$(tput setaf 3)
+normal=$(tput sgr0)
+red=$(tput setaf 1; tput bold)
+green=$(tput setaf 2; tput bold)
 
 # Ensure this script is run as root
 if [ "$(id -u)" != "0" ]; then
@@ -12,13 +21,87 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-# Make a temp dir to toss install files in	
-mkdir -p /tmp/csf_install
+# Progress spinner function
+function _spinner() {
+    # 		$1 start/stop
+    # on start: $2 display message
+    # on stop : $2 process exit status
+    #           $3 spinner function pid (supplied from stop_spinner)
 
-# Grab the latest version of CSF for FREE!!! ...let's decompress and install it too
-wget -q -O /tmp/csf_install/csf.tgz http://www.configserver.com/free/csf.tgz
-tar -xf /tmp/csf_install/csf.tgz -C /tmp/csf_install
-cd /tmp/csf_install/csf && sh install.sh
+    local on_success="SUCCESS"
+    local on_fail="FAIL"
+
+    case $1 in
+        start)
+            # Calculate the column where spinner and status msg will be displayed
+            let column=$(tput cols)-${#2}
+            # Display message and position the cursor in $column column
+            printf "${2}"
+            printf "%${column}s"
+
+            # Start spinner
+            i=1
+            sp='\|/-'
+            delay=0.15
+
+            while :
+            do
+                printf "\b${sp:i++%${#sp}:1}"
+                sleep $delay
+            done
+            ;;
+        stop)
+            if [[ -z ${3} ]]; then
+                # Spinner isn't running
+                exit 1
+            fi
+
+            kill $3 > /dev/null 2>&1
+
+            # inform the user uppon success or failure
+            if [[ $2 -eq 0 ]]; then
+                printf "\b[${green}${on_success}${normal}]\n"
+                sleep 1
+            else
+                printf "\b[${red}${on_fail}${normal}]\n\n"
+		eval printf %.0s- '{1..'"${COLUMNS:-$(tput cols)}"\}; echo
+		error_title="${bold}Check Error Log${normal}"
+		printf "%*s\n" $(((${#error_title}+$(tput cols))/2)) "$error_title"
+		eval printf %.0s- '{1..'"${COLUMNS:-$(tput cols)}"\}; echo
+		tail -2 install.log
+		printf "\n"
+		exit 1
+            fi
+            ;;
+        *)
+            # Invalid argument
+            exit 1
+            ;;
+    esac
+}
+
+function start_spinner {
+    # $1 : Msg to display
+    _spinner "start" "${1}" &
+    # Set global spinner pid
+    _sp_pid=$!
+    disown
+}
+
+function stop_spinner {
+    # $1 : Command exit status
+    _spinner "stop" $1 $_sp_pid
+    unset _sp_pid
+}
+
+# Remove previous install log
+if [[ -f "install.log" ]]; then
+	rm -f install.log
+fi
+
+start_spinner "${bold}Installing Dependencies${normal}"
+
+{
 
 # Install the CSF dependencies
 if [ -f /etc/redhat-release ]; then
@@ -26,6 +109,27 @@ if [ -f /etc/redhat-release ]; then
 else
 	apt-get -y update && apt-get -y install apache2 sed openssl libio-socket-ssl-perl libcrypt-ssleay-perl libnet-libidn-perl libio-socket-inet6-perl libsocket6-perl
 fi
+} &> install.log
+
+stop_spinner $?
+
+start_spinner "${bold}Installing Latest Version of CSF${normal}"
+
+{
+# Make a temp dir to toss install files in	
+mkdir -p /tmp/csf_install
+
+# Grab the latest version of CSF for FREE!!! ...let's decompress and install it too
+wget -q -O /tmp/csf_install/csf.tgz http://www.configserver.com/free/csf.tgz
+tar -xf /tmp/csf_install/csf.tgz -C /tmp/csf_install
+cd /tmp/csf_install/csf && sh install.sh
+} &> install.log
+
+stop_spinner $?
+
+start_spinner "${bold}Configuring CSF${normal}"
+
+{
 
 # Extra security stuffs because we don't like warning messages, view readme @ http://configserver.com/free/csf/readme.txt
 sed -i -e 's|RESTRICT_SYSLOG = "0"|RESTRICT_SYSLOG = "3"|g' /etc/csf/csf.conf
@@ -33,73 +137,111 @@ sed -i -e 's|RESTRICT_SYSLOG = "0"|RESTRICT_SYSLOG = "3"|g' /etc/csf/csf.conf
 # We'll just flip this switch off, we don't wanna test give us security now!
 sed -i -e 's|TESTING = "1"|TESTING = "0"|g' /etc/csf/csf.conf
 
-# Turn that GUI on
+# Turn that UI on
 sed -i -e 's|UI = "0"|UI = "1"|g' /etc/csf/csf.conf
+} &> install.log
 
-# Do we want to harden access to the CSF GUI based on IP?
-printf "\n${alert}${bold}Attention:${normal} Highly recommended that you restrict IP access to the GUI";
-printf "\n${info}${bold}Note:${normal} This restricts access based on client IP not server IP";
-printf "\n${info}${bold}Note:${normal} If you're behind a dynamic IP then you may want to decline\n";
-read -e -p "Do you wish to enable IP access restrictions? (y/n)" yn
+stop_spinner $?
+
+# Do we want to harden access to the CSF UI based on IP?
+# printf "\n${alert}${bold}Attention:${normal} Highly recommended that you restrict IP access to the UI";
+# printf "\n${info}${bold}Note:${normal} This restricts access based on client IP not server IP";
+# printf "\n${info}${bold}Note:${normal} If you're behind a dynamic IP then you may want to decline\n";
+tput sc
+read -e -p "Do you wish to enable UI IP access restrictions? (y/n)" yn
 case $yn in
 [Yy]* )
 	# Just in case you want to switch the option from the installer
 	sed -i -e 's|UI_ALLOW = "0"|UI_ALLOW = "1"|g' /etc/csf/csf.conf
-	# Giz me your IP address!... the one you're on now because it's the only one that will have access to the CSF GUI
-	printf "\n${alert}${bold}Attention:${normal} This is the only IP allowed access to the CSF GUI"
-	printf "\n${info}${bold}Note:${normal} You can add/remove IP's @ /etc/csf/ui/ui.allow\n"
-	read -e -p "CSF GUI Allowed Access IP: " -i "${SSH_CLIENT%% *}" csfIP
+	tput rc; tput el
+	printf "UI IP Restriction: [ ${green}ON ]\n"
+	# Giz me your IP address!... the one you're on now because it's the only one that will have access to the CSF UI
+	# printf "\n${alert}${bold}Attention:${normal} This is the only IP allowed access to the CSF UI"
+	# printf "\n${info}${bold}Note:${normal} You can add/remove IP's @ /etc/csf/ui/ui.allow\n"
+	read -e -p "CSF UI Allowed Access IP: " -i "${SSH_CLIENT%% *}" csfIP
 	echo ${csfIP} >> /etc/csf/ui/ui.allow
+	printf "\n"
 	;;
 [Nn]* ) 
+	tput rc; tput el
+	printf "UI IP Restriction: [ ${red}OFF ]\n"
 	sed -i -e 's|UI_ALLOW = "1"|UI_ALLOW = "0"|g' /etc/csf/csf.conf
 	;;
 esac
 
-# Giz me your GUI username!
+# Giz me your UI username!
 # Also this cannot be "username" or CSF will complain
-printf "\n${info}${bold}Note:${normal} You can edit this username @ /etc/csf/csf.conf\n"
-read -e -p "CSF GUI Login Username: " csfUser
+# printf "\n${info}${bold}Note:${normal} You can edit this username @ /etc/csf/csf.conf\n"
+read -e -p "CSF UI Login Username: " csfUser
 sed -i -e '/UI_USER/s/"\([^"]*\)"/"'${csfUser}'"/' /etc/csf/csf.conf
+printf "\n"
 
-# Giz me your GUI password!
+# Giz me your UI password!
 # You'll want this one strong although CSF has built in brute force detection (4 attempts)
 # Also this cannot be "password" or CSF will complain
-printf "\n${info}${bold}Note:${normal} You can edit this password @ /etc/csf/csf.conf\n"
-read -e -p "CSF GUI Login Password: " csfPass
+# printf "\n${info}${bold}Note:${normal} You can edit this password @ /etc/csf/csf.conf\n"
+while true
+do
+	tput sc
+	read -es -p "Password: " password
+	tput rc; tput el
+	read -es -p "Password (again): " csfPass
+	tput rc; tput el
+	[ "$password" = "$csfPass" ] && break
+	printf "Passwords do not match, try again"
+	sleep 1
+	tput rc; tput el
+done
+# read -e -p "CSF UI Login Password: " csfPass
 sed -i -e '/UI_PASS/s/"\([^"]*\)"/"'${csfPass}'"/' /etc/csf/csf.conf
+tput rc; tput el
+printf "CSF UI Password: [ ${green}OK ]\n"
 
 # Wanna get some emails from CSF?
-printf "\n${info}${bold}Note:${normal} Leave this blank to disable firewall activity alerts"
-printf "\n${info}${bold}Note:${normal} You can edit this email @ /etc/csf/csf.conf\n"
+# printf "\n${info}${bold}Note:${normal} Leave this blank to disable firewall activity alerts"
+# printf "\n${info}${bold}Note:${normal} You can edit this email @ /etc/csf/csf.conf\n"
 read -e -p "CSF Alert Email: " csfEmail
 sed -i -e '/LF_ALERT_TO/s/"\([^"]*\)"/"'${csfEmail}'"/' /etc/csf/csf.conf
+printf "\n"
 
-# Let's setup a port to push the GUI through
-printf "\n${info}${bold}Note:${normal} Should be >1023 and an unused port"
-printf "\n${info}${bold}Note:${normal} You can edit this port @ /etc/csf/csf.conf\n"
-read -e -p "CSF GUI Port: " csfPort
+# Let's setup a port to push the UI through
+# printf "\n${info}${bold}Note:${normal} Should be >1023 and an unused port"
+# printf "\n${info}${bold}Note:${normal} You can edit this port @ /etc/csf/csf.conf\n"
+read -e -p "CSF UI Port: " csfPort
 sed -i -e '/UI_PORT/s/"\([^"]*\)"/"'${csfPort}'"/' /etc/csf/csf.conf
 sed -i -e 's|TCP_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995"|TCP_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995,'${csfPort}'"|g' /etc/csf/csf.conf
+printf "\n"
 
+start_spinner "${bold}Removing Installation Files & APF+BFD${normal}"
+
+{
 # Just in case you were using APF+BFD we'll try to remove it if it exists
 sh /etc/csf/remove_apf_bfd.sh
 
 # Remove the temp install dir
 rm -rf /tmp/csf_install
+} &> install.log
 
-# Install SSL if you say we need to (we need it for GUI access)
-printf "\n${alert}${bold}Attention:${normal} SSL is ${bold}required${normal} for connecting to the CSF GUI\n";
-read -e -p "Do you need SSL installed and configured? (y/n)" yn
+stop_spinner $?
+
+# Install SSL if you say we need to (we need it for UI access)
+# printf "\n${alert}${bold}Attention:${normal} SSL is ${bold}required${normal} for connecting to the CSF UI\n";
+tput sc
+read -e -p "Do you need SSL installed and configured (required for UI)? (y/n)" yn
 case $yn in
 [Yy]* ) 
-	echo "Installing and configuring SSL for CSF GUI access..."
+	tput rc; tput el
+	
+	start_spinner "${bold}Installing OpenSSL & Configuring${normal}"
+
+	{
+	# echo "Installing and configuring SSL for CSF UI access..."
 	if [ -f /etc/redhat-release ]; then
 		yum -y install mod_ssl
 		cd /etc/csf/ui
 		openssl genrsa -out server.key 2048
 		openssl req -key server.key -new -out server.csr
-		openssl x509 -in server.csr -out server.crt -req -signkey server.key -days 3650
+		openssl x509 -subj "/C=US/ST=WA/L=Seattle/O=NA/CN=www.website.com" -in server.csr -out server.crt -req -signkey server.key -days 3650
 		chmod 400 server.*
 		cp server.key /etc/pki/tls/certs/server.key
 		cp server.crt /etc/pki/tls/certs/server.crt
@@ -107,7 +249,7 @@ case $yn in
 		sed -i -e 's|SSLCertificateKeyFile /etc/pki/tls/private/localhost.key|SSLCertificateKeyFile /etc/pki/tls/certs/server.key|g' /etc/httpd/conf.d/ssl.conf
 	else
 		cd /etc/csf/ui
-		openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -out server.crt -keyout server.key
+		openssl req -x509 -subj "/C=US/ST=WA/L=Seattle/O=NA/CN=www.website.com" -nodes -days 3650 -newkey rsa:2048 -out server.crt -keyout server.key
 		chmod 400 server.*
 		mkdir /etc/apache2/ssl/
 		a2enmod ssl
@@ -132,21 +274,37 @@ case $yn in
 		SSLCertificateKeyFile /etc/apache2/ssl/server.key
 		</virtualhost>" >> /etc/apache2/sites-available/${domain}.conf
 	fi
+	} &> install.log
+
+	stop_spinner $?
+	
 	;;
 [Nn]* ) 
-	printf "\n${alert}${bold}Attention:${normal} Make sure you copy your key and cert files to /etc/csf/ui\n";
-	read -p "Press ${bold}[Enter]${normal} to complete installation..."
+	# printf "\n${alert}${bold}Attention:${normal} Make sure you copy your key and cert files to /etc/csf/ui\n";
+	tput rc; tput el
+	printf "SSL Installation: [ ${red}NO ]\n"
 	;;
 esac
 
+start_spinner "${bold}Starting CSF+LFD${normal}"
+
+{
 # Restart firewall and Apache
 if [ -f /etc/redhat-release ]; then
 	service httpd stop && service httpd start && csf -r && service lfd restart
 else
 	service apache2 stop && service apache2 start && csf -r && service lfd restart
 fi
+} &> install.log
+
+stop_spinner $?
+
+printf "\n"
 
 # Let's run a quick test make sure we don't have any fatal errors
 perl /usr/local/csf/bin/csftest.pl
+
+# Restor cursor
+tput cnorm
 
 
